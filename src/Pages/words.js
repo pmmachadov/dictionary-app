cf22616812bc44c3dd653426f64817a64f3d8483
pmmachadov <pmmachadov@gmail.com>
2023-10-24 22:25:04 +0200

    Lista de muestra en Catalan

--- src/Pages/words.js
+++ src/Pages/words.js
@@ -1,7 +1,14 @@
 const words = [
-  { word: 'apple', definition: 'A round, red or green fruit that is eaten as food.' },
-  { word: 'banana', definition: 'A long, yellow fruit that is eaten as food.' },
-  { word: 'orange', definition: 'A round, orange fruit that is eaten as food.' },
+  { word: 'poma', definition: 'Fruit rodó, de color vermell o verd, que es menja com a aliment.' },
+  { word: 'plàtan', definition: 'Fruit allargat, de color groc, que es menja com a aliment.' },
+  { word: 'taronja', definition: 'Fruit rodó, de color taronja, que es menja com a aliment.' },
+  { word: 'gat', definition: 'Un animal domèstic, sovint pelut, que és popular com a company.' },
+  { word: 'muntanya', definition: 'Un accident geogràfic que sol elevar-se per sobre del terreny circumdant.' },
+  { word: 'llibre', definition: 'Un conjunt de pàgines escrites i enquadernades que conté informació o històries.' },
+  { word: 'pluja', definition: 'Aigua que cau del cel en forma de gotes líquides.' },
+  { word: 'amor', definition: "Un fort sentiment d'afecte i apreci que una persona sent cap a una altra." },
+  { word: 'família', definition: 'Un grup de persones emparentades que viuen juntes.' },
+  { word: 'amistat', definition: 'Una relació afectuosa i de suport entre amics.' }
 ];
 
 export default words;
